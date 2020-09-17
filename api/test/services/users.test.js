d602a08a64e36e07d751a67a8f7105e2ab910298
Guillaume Bouillé <guillaume@jiminee.club>
2020-09-17 14:27:42 +0200

    Setup api with feathers and eslint

---
+++ api/test/services/users.test.js
@@ -0,0 +1,9 @@
+const app = require('../../src/app')
+
+describe('\'users\' service', () => {
+  it('registered the service', () => {
+    const service = app.service('users')
+
+    expect(service).toBeTruthy()
+  })
+})
